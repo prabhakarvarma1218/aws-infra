58691e05c81d4faf312994a3599897d43b520386
prabhakarvarma1218 <127718684+prabhakarvarma1218@users.noreply.github.com>
2024-02-05 12:35:30 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,112 @@
+provider "aws" {
+  access_key = "Access_key"
+  secret_key = "Secrete_key"
+  region  = "us-east-1"
+}
+ 
+resource "aws_vpc" "some_custom_vpc" {
+  cidr_block = "10.0.0.0/16"
+ 
+  tags = {
+    Name = "terraform vpc"
+  }
+}
+ 
+resource "aws_subnet" "some_public_subnet" {
+  vpc_id            = aws_vpc.some_custom_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+ 
+  tags = {
+    Name = "terraform public Subnet"
+  }
+}
+ 
+resource "aws_subnet" "some_private_subnet" {
+  vpc_id            = aws_vpc.some_custom_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+ 
+  tags = {
+    Name = "terraform Private Subnet"
+  }
+}
+ 
+resource "aws_internet_gateway" "some_ig" {
+  vpc_id = aws_vpc.some_custom_vpc.id
+ 
+  tags = {
+    Name = "terraform Internet Gateway"
+  }
+}
+ 
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.some_custom_vpc.id
+ 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.some_ig.id
+  }
+ 
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.some_ig.id
+  }
+ 
+  tags = {
+    Name = "terraform Route Table"
+  }
+}
+ 
+resource "aws_route_table_association" "public_1_rt_a" {
+  subnet_id      = aws_subnet.some_public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+ 
+resource "aws_security_group" "web_sg" {
+  name   = "terraform sg"
+  vpc_id = aws_vpc.some_custom_vpc.id
+ 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+ 
+resource "aws_instance" "web_instance" {
+  ami           = "ami-0533f2ba8a1995cf9"
+  instance_type = "t2.nano"
+  key_name      = "my_key"
+ 
+  subnet_id                   = aws_subnet.some_public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+ 
+  user_data = <<-EOF
+  #!/bin/bash -ex
+ 
+  amazon-linux-extras install nginx1 -y
+  echo "<h1>Welcome to nginx</h1>" >  /usr/share/nginx/html/index.html
+  systemctl enable nginx
+  systemctl start nginx
+  EOF
+ 
+  tags = {
+    "Name" : "terraform_vm"
+  }
+}
